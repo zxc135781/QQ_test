b3878ab0885c6f6c4c9f6f5e35f7743e9fed56ab
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-03 07:03:26 +0000

    Deploy zxc135781/QQ_test to github.com/zxc135781/QQ_test.git:gh-pages

---
+++ qq.py
@@ -0,0 +1,15 @@
+import requests
+import yaml
+
+r=requests.get('http://task.qq.com/index.php/taskListContent?&pageSize=10&pageNumber=1')
+a = r.json()
+print(a['data']['taskList'])
+
+with open('father.yml') as f:
+    content = yaml.load(f,Loader=yaml.FullLoader)
+    print(type(content))
+    print(content)
+    content.update({'age': 39})
+    print(content)
+    with open('father.yml', 'w') as nf:
+        yaml.dump(content, nf)
