c7435fddfb50a58dd199d13d2f446912c3e4e2b0
Sihua <827169070@qq.com>
2019-11-03 16:02:31 +0800

    2019.11.03

--- qq.py
+++ qq.py
@@ -1,15 +1,23 @@
+# -*- coding: utf-8 -*-
 import requests
 import yaml
 
 r=requests.get('http://task.qq.com/index.php/taskListContent?&pageSize=10&pageNumber=1')
 a = r.json()
-print(a['data']['taskList'])
-
-with open('father.yml') as f:
-    content = yaml.load(f,Loader=yaml.FullLoader)
-    print(type(content))
-    print(content)
-    content.update({'age': 30})
-    print(content)
-    with open('father.yml', 'w') as nf:
-        yaml.dump(content, nf)
+b= a['data']['taskList']
+for index in range(len(b)):
+    # print(b[index]['name'])
+    if (b[index]['product']== 'com.tencent.mobileqq' and b[index]['expireStatus']==True):
+        print(b[index]['versionBuild'])
+        with open('father.yml') as f:
+            content = yaml.load(f, Loader=yaml.FullLoader)
+            print(content)
+            content.update(
+                {
+                    'product': b[index]['product'],
+                    'versionBuild': b[index]['versionBuild'],
+                    'endTime': b[index]['endTime']
+                })
+            print(content)
+            with open('father.yml', 'w') as nf:
+                yaml.dump(content, nf)
